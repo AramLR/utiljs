1b60110e60b40134c41066c6507860809f241d0d
AramLR <aramluck2007@gmail.com>
2022-06-25 20:37:34 -0500

    add docs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+/**
+ * Capitalizes a string
+ * 
+ * @param {string} str String to capitalize
+ * @returns {string} A capitalized version of the string
+ */
 function capitalize(str:string){
+
   const result = str.charAt(0).toUpperCase() + str.substring(1).toLowerCase()
 
   return result
